81b13a6f2efc3949c50b8ddc9a7d777639808ad6
luluissaa <luisa.lyji@gmail.com>
2020-03-11 23:09:06 -0400

    fixed bottom

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ const SecondPage = () => (
 <div className="intro">
 <div>
   <h1>about</h1>
-<p>Luisa is a Chinese-Canadian creative strategist, digital designer, and artist with the approach of an architect and the eyes of a photographer. 
+<p>Luisa is a Chinese-Canadian creative strategist, designer-futurist, and artist with the approach of an architect and the eyes of a photographer. Her work span disciplines including art, architecture, culture, and how to visualize and make sense of the future. With broad interests and curiosity, Luisa worked on projects focused on digitally-mediated civic engagement, participatory urban design, and performative work based on spatial interactions alongside artists, architects, and socially-engaged designers in New Caledonia, U.S., and Canada. Luisa holds a master's degree in Architecture and is the global community director of Speculative Futures, a network of designer-futurists and foresight practitioners in 40+ cities worldwide using their creative practices to activate social change.
 </p>
 <Link to="//">go back</Link>
 
@@ -43,7 +43,9 @@ const SecondPage = () => (
     <h1>Ladies Wine & Design Ottawa</h1>
   </div>
 </div>
-
+      <nav>
+        <Link to="/projects/">projects</Link>
+      </nav>
 </div>
   </Layout>
 )
