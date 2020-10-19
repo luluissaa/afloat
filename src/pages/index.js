9e10b4670245a9a84469439363c352fd5e3e21b0
luluissaa <luisa.lyji@gmail.com>
2020-10-19 01:11:12 -0400

    update cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import Aside from "../components/aside"
 import Introduction from "../components/intro"
 
