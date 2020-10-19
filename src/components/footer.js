37188440c4e8b89b7528cf88f58cfec1d2763860
luluissaa <luisa.lyji@gmail.com>
2020-10-19 01:04:39 -0400

    add cv link

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ const Footer = ({ siteFooter }) => (
   <footer>
     <p>© {new Date().getFullYear()} AfloatingSpace </p>
           <p>luisa[~at~]afloating.space</p>
-          <p><a href="https://www.twitter.com/luisalyji" target="_blank" rel="noopener">@luisalyji twitter</a></p>
-          <p><a href="https://www.instagram.com/luisalyji" target="_blank" rel="noopener">@luisalyji instagram</a></p>
+          <p><a href="https://www.twitter.com/luisalyji" target="_blank" rel="noopener noreferrer">@luisalyji twitter</a></p>
+          <p><a href="https://www.instagram.com/luisalyji" target="_blank" rel="noopener noreferrer">@luisalyji instagram</a></p>
   </footer>
 )
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,11 +10,11 @@ const SecondPage = () => (
 
       <div className="intro">
             <div>
-              <h1>about</h1>
+            <h1>about</h1>
             <p>Luisa is a Chinese-Canadian creative strategist, designer-futurist, and artist with the approach of an architect and the eyes of a photographer. Her work span disciplines including art, architecture, culture, and how to visualize and make sense of the future. With broad interests and curiosity, Luisa worked on projects focused on digitally-mediated civic engagement, participatory urban design, and performative work based on spatial interactions alongside artists, architects, and socially-engaged designers in New Caledonia, U.S., and Canada. Luisa holds a master's degree in Architecture and is the global community director of Speculative Futures, a network of designer-futurists and foresight practitioners in 40+ cities worldwide using their creative practices to activate social change.
             </p>
+            <h4><a href="https://www.notion.so/4ac8a95f06ca411c82cd87ddf933f2e3?v=58e8002b251542968d5a396bc1e7d6ac" target="_blank" rel="noopener noreferrer">cv & headshots</a></h4>
             <div>
-            <h4><a href="">cv</a> / <a href="">headshots</a></h4>
             </div>
             </div>
             <Link to="/">back</Link>
